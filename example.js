b59a85b2709e9978d0bdd99cf8122745d88d51ce
Ivan Kozik <ivan@ludios.org>
2015-05-25 03:26:09 +0000

    Revert "Don't use incorrect 'const' in for-of loop"
    This reverts commit 6915e87acceacd5483feeb22ab743cf9d0b2b54d.
    
    `const` is fine in for-of loops; they have fresh bindings each iteration

--- example.js
+++ example.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const co = require('co');
 
 co(function*() {
-	for(let chunkStream of chunk(fs.createReadStream('/etc/passwd'), 100)) {
+	for(const chunkStream of chunk(fs.createReadStream('/etc/passwd'), 100)) {
 		chunkStream.on('data', function(data) {
 			process.stdout.write(data);
 			console.log("\n" + data.length + "\n");
